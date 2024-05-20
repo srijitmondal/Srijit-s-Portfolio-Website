2befc4f5d2348739c9361c74e54745855ecf773a
SRIJIT MONDAL <125857548+srijitmondal@users.noreply.github.com>
2024-05-20 10:42:22 +0530

    Initial upload

---
+++ main.js
@@ -0,0 +1,54 @@
+// Scroll reveal
+ScrollReveal({
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+// ScrollReveal to reveal elements
+ScrollReveal().reveal('.home-content,.heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-contact h1,.about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p,.about-content', { origin: 'right' });
+ScrollReveal().reveal('.navbar a', { origin: 'top', delay: 300 });
+ScrollReveal().reveal('.social-media a', { origin: 'bottom', delay: 500 });
+ScrollReveal().reveal('.btn', { origin: 'left', delay: 700 });
+
+/*======= Toggle icon behaviour =======*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+/*======= Scroll section active =======*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector(`header nav a[href*='${id}']`).classList.add('active');
+            });
+        }
+    });
+    
+
+/*======= Sticky navbar=======*/
+let header = document.querySelector('header');
+header.classList.toggle('sticky',window.scrollY > 100);
+
+/*======= remove toggleicon & navbar =======*/
+menuIcon.classList.remove('fa-xmark');
+navbar.classList.remove('active');
+
+};
